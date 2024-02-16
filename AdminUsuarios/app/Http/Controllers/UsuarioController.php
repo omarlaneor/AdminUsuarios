a8aa6a94f7f9aa943b447c6e6d92d7a68322836c
omarlaneor <annielaneor@gmail.com>
2024-02-16 18:26:57 -0500

    first commit

---
+++ AdminUsuarios/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Usuario::create($request->all());
+        return ['created'];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return Usuario::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->update($request->all());
+
+        return ['Successfully updated'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $usuario = Usuario::find($id);
+        $usuario->delete($id);
+
+        return ['Successfully Deleted!'];
+    }
+}
